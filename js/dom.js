7fcf06db5148153ca19af251d8c44dac81aab2d0
Mintu Sikder <mintusikder15@gmail.com>
2025-02-21 21:37:16 +0600

    change folder

--- js/dom.js
+++
@@ -1,5 +0,0 @@
-// console.log("hello form js")
-// console.log(document.body)
-
-const sections = document.querySelectorAll("section")
-console.log(sections)
---
+++ tourOfDom/js/dom.js
@@ -0,0 +1,12 @@
+// console.log("hello form js")
+// console.log(document.body)
+
+const sections = document.querySelectorAll("section");
+console.log(sections);
+
+for (const section of sections) {
+  section.style.border = "2px solid red";
+  section.style.marginBottom = " 5px";
+  section.style.borderRadius = "5px";
+  section.style.padding = "10px";
+}
