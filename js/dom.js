c601ff7ec7323d224e81ef8f391800a575b18f17
Mintu Sikder <mintusikder15@gmail.com>
2025-02-21 21:29:02 +0600

    added title

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,5 @@
-console.log("hello form js")
-console.log(document.body)
+// console.log("hello form js")
+// console.log(document.body)
+
+const sections = document.querySelectorAll("section")
+console.log(sections)
