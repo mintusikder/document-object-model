35bbec56fb4c3382150d3e1a0ab200b08ec9db23
Mintu Sikder <mintusikder15@gmail.com>
2025-02-23 09:39:22 +0600

    added event 4 option

--- domEvent/js/event.js
+++ domEvent/js/event.js
@@ -1,8 +1,22 @@
 // option 2 js
 function makeYellow() {
-    document.body.style.backgroundColor = "yellow";
-  }
+  document.body.style.backgroundColor = "yellow";
+}
 
-  function makeRed() {
-    document.body.style.backgroundColor = "red";
-  }
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+// option 3 : get element by id
+
+const btnMakeBlue = document.getElementById("btn-make-blue");
+btnMakeBlue.onclick = function btnMakeBlue() {
+  document.body.style.backgroundColor = "blue";
+};
+// option  3 different 
+
+const btnMakePurple = document.getElementById("make-purple")
+btnMakePurple.onclick = makePurple
+function makePurple(){
+   document.body.style.backgroundColor = "purple";
+}
