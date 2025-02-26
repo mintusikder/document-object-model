5dc657f53f4e896851520b87d6b4f55f163326ba
Mintu Sikder <mintusikder15@gmail.com>
2025-02-26 18:52:37 +0600

    added main nav section

--- bankProject/script/login.js
+++ bankProject/script/login.js
@@ -4,8 +4,9 @@ document.getElementById("login-btn").addEventListener("click", function (event) 
     const accNumber = document.getElementById("acc-number").value 
     const accPin = document.getElementById("acc-pin").value 
     if(accNumber.length === 11){
-        if(parseInt(accPin) === 1111){
+        if(parseInt(accPin) === 1234){
             console.log("ok thik ache")
+            window.location.href = "../main.html";
         }else{
             console.log("pin thik nei")
         }
