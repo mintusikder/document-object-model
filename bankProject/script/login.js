0efbfe4a9de4ffcf79b86835d754f656580cec49
Mintu Sikder <mintusikder15@gmail.com>
2025-02-26 19:36:19 +0600

    added add money form

--- bankProject/script/login.js
+++ bankProject/script/login.js
@@ -1,16 +1,18 @@
 // step 1: Get element by id
-document.getElementById("login-btn").addEventListener("click", function (event) {
-    event.preventDefault()
-    const accNumber = document.getElementById("acc-number").value 
-    const accPin = document.getElementById("acc-pin").value 
-    if(accNumber.length === 11){
-        if(parseInt(accPin) === 1234){
-            console.log("ok thik ache")
-            window.location.href = "../main.html";
-        }else{
-            console.log("pin thik nei")
-        }
-    }else{
-        console.log("need validate account number")
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const accNumber = document.getElementById("acc-number").value;
+    const accPin = document.getElementById("acc-pin").value;
+    if (accNumber.length === 11) {
+      if (parseInt(accPin) === 1234) {
+        alert("ok thik ache");
+        window.location.href = "../main.html";
+      } else {
+        alert("pin thik nei");
+      }
+    } else {
+      alert("need validate account number");
     }
-});
+  });
