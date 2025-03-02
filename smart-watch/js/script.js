ef0d20a3438dda63d11795c0511df8e703b68bae
Mintu Sikder <mintusikder15@gmail.com>
2025-03-02 23:59:21 +0600

    add to cart

--- smart-watch/js/script.js
+++ smart-watch/js/script.js
@@ -46,3 +46,17 @@ for (const btn of quantityElement) {
     quantityElement.innerText = newQuantity;
   });
 }
+
+//add to cart
+let cartCount = 0;
+document.getElementById("add-to-cart").addEventListener("click", function () {
+  const quantity = parseInt(document.getElementById("quantity").innerText);
+
+  if (quantity > 0) {
+    document.getElementById("checkout-container").classList.remove("hidden");
+    cartCount = cartCount + quantity;
+    document.getElementById("cart-count").innerText = cartCount;
+  } else {
+    alert("Please Select a quantity");
+  }
+});
